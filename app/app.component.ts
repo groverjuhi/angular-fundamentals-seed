98204e548d4e3e62affc55c3b92d8c709a0754a2
Thakur Grover <thakur@Thakurs-MacBook-Pro.local>
2018-08-25 14:34:55 +0200

    my changes to learn binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,88 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      Hello!
+    <h1>{{ title }}</h1>
+    <img [src] = 'logo'>
+      <div>
+        <input 
+          type="text"
+          [value]="name"
+          (input)="handleInput($event)"
+          (blur)="handleBlur($event)">
+      </div>
+      <div>
+        {{ name }}
+      </div>
+      <div>
+        <button (click)="handleClick()">
+        change name
+        </button>
+      </div>
+
+      <!-- Creating seperate div for understanding ng module property and event binding -->
+
+      <div>
+      <input 
+        type="text"
+        [ngModel]="nameNew"
+        (ngModelChange)="handleChange($event)">
+        <input 
+        type="text"
+        [(ngModel)]="nameNew">
+    </div>
+    <div>
+      {{ nameNew }}
+    </div>
+
+      <!-- Testing Refrence -->
+      
+      <div>
+        <input
+        type = "text" #username>
+        <button (click)="getValueOnClick(username.value)">
+        Get Value
+        </button>
+      </div>
+
+      <div>
+      {{ nameUser }}
+      </div>
+
     </div>
   `
 })
+
 export class AppComponent {
+
+  title: string;
+  logo: string;
+  name: string;
+  nameNew: string;
+  nameUser: string;
+
+  handleInput(event: any){
+    this.name = event.target.value;
+  }
+  handleBlur(event: any){
+    // this.name = event.target.value;
+    this.name = 'JUHI';
+  }
+  handleClick(){
+    this.name = 'JUHI GROVER';
+  }
+  handleChange(value: string) {
+    this.nameNew = value;
+  }
+  getValueOnClick(value: string){
+    this.nameUser = value;
+  }
+  constructor() {
+    this.title = 'Hello Ultimate Angular';
+    this.logo = 'img/Hindi_swar_mala.jpg';
+    this.name = 'thakur';
+    this.nameNew = 'juhi';
+    this.nameUser = '';
+
+  }
 
 }
