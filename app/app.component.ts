604a4e30f04ee8561adb536334646d4266e72a81
Thakur Grover <thakur@Thakurs-MacBook-Pro.local>
2018-08-26 14:57:20 +0200

    changes for ngFor

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,117 +1,54 @@
 import { Component } from '@angular/core';
 
+interface Passenger{
+id: number;
+fullname: string;
+checkedIn: boolean;
+}
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-    <h1>{{ title }}</h1>
-    <img [src] = 'logo'>
-      <div>
-        <input 
-          type="text"
-          [value]="name"
-          (input)="handleInput($event)"
-          (blur)="handleBlur($event)">
-      </div>
-      <div>
-        {{ name }}
-      </div>
-      <div>
-        <button (click)="handleClick()">
-        change name
-        </button>
-      </div>
-
-      <!-- Creating seperate div for understanding ng module property and event binding -->
-
-      <div>
-      <input 
-        type="text"
-        [ngModel]="nameNew"
-        (ngModelChange)="handleChange($event)">
-        <input 
-        type="text"
-        [(ngModel)]="nameNew">
-    </div>
-    <div>
-      {{ nameNew }}
-    </div>
-
-      <!-- Testing Refrence -->
-      
-      <div>
-        <input
-        type = "text" #username>
-        <button (click)="getValueOnClick(username.value)">
-        Get Value
-        </button>
-      </div>
-
-      <div>
-      {{ nameUser }}
-      </div>
-
-      <!-- Testing show and hide behaviour using ngIf -->
-      
-      <div>
-        <input
-        type = "text"
-        [value]="nameUser"
-        (input)="handleInputCheck($event.target.value)">
-
-      </div>
-
-      <div *ngIf="nameUser.length > 2">
-      Searching for .... {{ nameUser }}
-      </div>
-
-      <template [ngIf]="nameUser.length > 2">
-      <div>
-      Searching for .... {{ nameUser }}
-      </div>
+    <h3>Airline Passengers</h3>
+    <ul>
+      <template ngFor let-passenger let-i="index" [ngForOf]="passengers">
+        <li>
+         {{ i }}: {{ passenger.fullname}}
+         <!-- passenger list -->
+        </li>
       </template>
+    </ul>
 
-
+    <h3>Airline Passengers</h3>
+    <ul>
+      <li *ngFor="let passenger of passengers; let i = index;">
+      {{ i }}: {{ passenger.fullname}}
+        <!-- passenger list -->
+      </li>
+    </ul>
     </div>
   `
 })
 
 export class AppComponent {
-
-  title: string;
-  logo: string;
-  name: string;
-  nameNew: string;
-  nameUser: string;
-
-  handleInput(event: any){
-    this.name = event.target.value;
-  }
-  handleBlur(event: any){
-    // this.name = event.target.value;
-    this.name = 'JUHI';
-  }
-  handleClick(){
-    this.name = 'JUHI GROVER';
-  }
-  handleChange(value: string) {
-    this.nameNew = value;
-  }
-  getValueOnClick(value: string){
-    this.nameUser = value;
-  }
-
-  handleInputCheck(value: string){
-    this.nameUser = value;
-  }
-  constructor() {
-    this.title = 'Hello Ultimate Angular';
-    this.logo = 'img/Hindi_swar_mala.jpg';
-    this.name = 'thakur';
-    this.nameNew = 'juhi';
-    this.nameUser = '';
-
-  }
-
+passengers: Passenger[] = [{
+  id: 1,
+  fullname: 'Arnav',
+  checkedIn: true
+  }, {
+  id: 2,
+  fullname: 'Juhi',
+  checkedIn: true
+  }, {
+  id: 3,
+  fullname: 'Abhinav Grover',
+  checkedIn: false
+  }, {
+  id: 4,
+  fullname: 'Jyoti',
+  checkedIn: true
+  } 
+]
 }
